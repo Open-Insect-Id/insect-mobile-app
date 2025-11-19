43aa32f5e4fd88232015a1381054cfdfe31f4897
Lucky <85549951+LuckyTheCookie@users.noreply.github.com>
2025-11-19 11:52:17 +0100

    Testing Camera implementation

--- src/main.py
+++ src/main.py
@@ -9,14 +9,42 @@ def main(page: ft.Page):
         counter.value = str(counter.data)
         counter.update()
 
+    picker = ft.FilePicker()
+    page.overlay.append(picker)
+
+    def on_picker_result(e: ft.FilePickerResultEvent):
+        if e.files:
+            image = ft.Image(src_base64=e.files[0].data, width=300, height=300)
+            page.add(image)
+
+    picker.on_result = on_picker_result
+
+    take_photo_button = ft.ElevatedButton(
+        "Prendre une photo",
+        on_click=lambda e: picker.pick_files(
+            allow_multiple=False,
+            file_type=ft.FilePickerFileType.IMAGE,
+            with_data=True
+        )
+    )
+
     page.floating_action_button = ft.FloatingActionButton(
         icon=ft.Icons.ADD, on_click=increment_click
     )
     page.add(
         ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
+            ft.Column(
+                [
+                    ft.Text("Welcome", size=30, weight=ft.FontWeight.BOLD),
+                    take_photo_button,
+                    ft.Container(
+                        counter,
+                        alignment=ft.alignment.center,
+                    ),
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                expand=True,
             ),
             expand=True,
         )
