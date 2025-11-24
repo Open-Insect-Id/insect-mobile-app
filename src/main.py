fb06a6cc1c9826a4d03220e858e48accb669df97
Elnix90 <elnix91@gmail.com>
2025-11-24 17:00:22 +0100

    Removed photo related stuff as there are no available libes for this kind of stuff and it's not good in python

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import sys
+
 import flet as ft
 
 from utils.running_platform import running_platform
@@ -11,13 +13,11 @@ def main(page: ft.Page):
     page.overlay.append(ph)
     page.overlay.append(picker)
 
-    photo_permission_granted = False
     storage_permission_granted = False
-    supports_camera = True  # Have to replace  with real check
 
     current_platform = page.platform
 
-    if current_platform == ft.PagePlatform.IOS or current_platform == ft.PagePlatform.LINUX:
+    if current_platform == ft.PagePlatform.IOS or current_platform == ft.PagePlatform.MACOS:
         print("Cannot run on Apple software", file=sys.stderr)
         sys.exit(1)
 
@@ -40,34 +40,10 @@ def main(page: ft.Page):
         else:
             storage_permission_granted = ph.request_permission(ft.PermissionType.MANAGE_EXTERNAL_STORAGE)
 
-    def request_permission_and_take_photo(e):
-        """
-        Request android permission to access camera / take a photo
-        (RN it only picks a photo using the file picker, but it should take a photo in real time)
-        :param e: nothing
-        """
-        nonlocal photo_permission_granted
-        photo_permission_granted = ph.request_permission(ft.PermissionType.CAMERA)
-        if (photo_permission_granted and supports_camera) or current_platform != ft.PagePlatform.ANDROID:
-            picker.pick_files(
-                allow_multiple=False,
-                file_type=ft.FilePickerFileType.IMAGE,
-            )
-        else:
-            page.snack_bar = ft.SnackBar(ft.Text("Camera permission not granted or not supported"))
-            page.snack_bar.open = True
-            page.update()
-
     picker.on_result = on_picker_result
 
-    browse_photo_button = ft.ElevatedButton(
-        "Browse photo" if photo_permission_granted else " Grand storage permission",
-        on_click=browse_photo
-    )
-
-    take_photo_button = ft.ElevatedButton(
-        "Take photo" if photo_permission_granted else " Grant Camera permission",
-        on_click=request_permission_and_take_photo
+    page.floating_action_button = ft.FloatingActionButton(
+        icon=ft.Icons.PHOTO, on_click=browse_photo
     )
 
     page.add(
@@ -75,12 +51,6 @@ def main(page: ft.Page):
             ft.Column(
                 [
                     ft.Text(f"You are running {running_platform(page)}", size=30, weight=ft.FontWeight.BOLD),
-                    ft.Container(
-                        content = None,
-                        height=50
-                    ),
-                    take_photo_button,
-                    browse_photo_button
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
